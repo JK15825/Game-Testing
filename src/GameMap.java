5e81bc5fa417a2b23282fbcf7b693e13e58ec8f9
JK15825 <baseballranger@gmail.com>
2015-05-30 12:21:35 -0500

    oh noes i did something

--- src/GameMap.java
+++ src/GameMap.java
@@ -1,5 +1,17 @@
 
-public class GameBoard 
+public class GameMap 
 {
-
+	int map[][];
+	int screenWidth;
+	int screenHeight;
+	public GameMap(int width, int height)
+	{
+		 map = new int[width/8][height/8];
+		 screenWidth = width;
+		 screenHeight = height;
+	}
+	public void update()
+	{
+		
+	}
 }
--- src/GameThread.java
+++ src/GameThread.java
@@ -1,4 +1,19 @@
 
-public class GameThread {
-
+public class GameThread implements Runnable
+{
+	GameBoard board;
+	public GameThread(GameBoard board)
+	{
+		this.board = board;
+	}
+	@Override
+	public void run() 
+	{
+		while(true)
+		{
+			board.update();
+			board.render();
+		}
+	}
+	
 }
